27e887f2691d2db795f9ea6d262c843a3893edb8
Joakim Rosdahl Severinsen <jose0001@stud.ek.dk>
2025-09-30 15:52:28 +0200

    Rettet lidt snask + tilføjet et manglende import

--- src/main/java/org/example/adventurexp/repositories/IEquipmentRepository.java
+++ src/main/java/org/example/adventurexp/repositories/IEquipmentRepository.java
@@ -2,8 +2,10 @@ package org.example.adventurexp.repositories;
 
 import org.example.adventurexp.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface IEquipmentRepository extends JpaRepository<Equipment, Integer>{
-    @Override
+
+    // Returnerer en liste af Equipment ud fra ActivityID
     List<Equipment> findByActivityId(int activityId);
 }
