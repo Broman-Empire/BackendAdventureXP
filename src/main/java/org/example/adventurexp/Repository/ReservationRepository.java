353f5bbc8009f459765064a99c0be247ce56b1cd
Mellerkaer <rasmusmellerkaer@gmail.com>
2025-09-30 17:00:27 +0200

    Lavet ReservationRepository og ændret på model Reservation

---
+++ src/main/java/org/example/adventurexp/Repository/ReservationRepository.java
@@ -0,0 +1,12 @@
+package org.example.adventurexp.Repository;
+
+import dk.kea.adventurexp.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+
+
+}
