9d3bdfa7c438571c2dcb17c463da3626c526256d
Joakim Rosdahl Severinsen <jose0001@stud.ek.dk>
2025-09-30 16:47:35 +0200

    Fjernet unused import statements

--- src/main/java/org/example/adventurexp/controller/ActivityController.java
+++ src/main/java/org/example/adventurexp/controller/ActivityController.java
@@ -1,7 +1,5 @@
 package org.example.adventurexp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
