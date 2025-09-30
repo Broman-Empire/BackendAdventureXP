6204b663f75b79d42ff24ec0fde8933a5b7cae6a
Mellerkaer <rasmusmellerkaer@gmail.com>
2025-09-30 17:03:16 +0200

    Lavet ReservationRepository

--- src/main/java/org/example/adventurexp/Repository/ReservationRepository.java
+++ src/main/java/org/example/adventurexp/Repository/ReservationRepository.java
@@ -1,12 +1,15 @@
 package org.example.adventurexp.Repository;
 
-import dk.kea.adventurexp.model.Reservation;
+import org.example.adventurexp.model.Reservation;
+import org.kea.adventurexp.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+
+    Reservation findByReservationId(Long reservationId);
 
 
 }
