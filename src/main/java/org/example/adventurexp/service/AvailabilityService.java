66f6512c9c65f12a14f23ae0530a6af6c1404eee
Mellerkaer <rasmusmellerkaer@gmail.com>
2025-10-01 21:03:20 +0200

    AvailabilityService

--- src/main/java/org/example/adventurexp/service/AvailabilityService.java
+++ src/main/java/org/example/adventurexp/service/AvailabilityService.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 
 public interface AvailabilityService {
 
-    // Returner dagens tilgængelige slots for en aktivitet.
+    // Returner dagens tilgængelige slots for en aktivitet
     AvailabilityDTO[] getDailyAvailability(long activityId, LocalDate date);
 
 }
