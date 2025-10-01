83c55b679de8c534fa2e618094e8c67cb50a3fe2
Mellerkaer <rasmusmellerkaer@gmail.com>
2025-10-01 21:37:42 +0200

    chat forslag

--- src/main/java/org/example/adventurexp/service/AvailabilityServiceImpl.java
+++ src/main/java/org/example/adventurexp/service/AvailabilityServiceImpl.java
@@ -21,4 +21,13 @@ public class AvailabilityServiceImpl implements AvailabilityService {
         // returnerer et tomt array for nu
         return new AvailabilityDTO[0];
     }
+
+    //Chattens forslag til løsning:
+
+    // TODO: Slå slots op for datoen via TimeSlotRepository (startsAt mellem startOfDay og endOfDay)
+    // TODO: (Valgfrit) Slå Activity og Equipment op for at beregne effektiv kapacitet (min(slot.capacity, activity.maxParticipants, equipment.usableSets))
+    // TODO: Map hvert slot til AvailabilityDTO (start, end, capacity, remaining, soldOut)
+    // TODO: Filtrér slots hvor remaining <= 0 (hide optagede)
+    // TODO: Returnér som AvailabilityDTO[]
+
 }
