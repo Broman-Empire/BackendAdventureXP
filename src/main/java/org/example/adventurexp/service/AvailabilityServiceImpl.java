ffa123d2e991279cf7e34e189944f6b4b781b1d4
Mellerkaer <rasmusmellerkaer@gmail.com>
2025-10-01 21:23:01 +0200

    tilføjet TimeSlot til imports

--- src/main/java/org/example/adventurexp/service/AvailabilityServiceImpl.java
+++ src/main/java/org/example/adventurexp/service/AvailabilityServiceImpl.java
@@ -2,11 +2,19 @@ package org.example.adventurexp.service;
 
 import org.example.adventurexp.dto.AvailabilityDTO;
 import org.springframework.stereotype.Service;
+import org.example.adventurexp.model.TimeSlot;
+import org.example.adventurexp.repository.TimeSlotRepository;
 
 import java.time.LocalDate;
 
 @Service
 public class AvailabilityServiceImpl implements AvailabilityService {
+
+    private final TimeSlotRepository timeSlotRepository;
+
+    public AvailabilityServiceImpl(TimeSlotRepository timeSlotRepository) {
+        this.timeSlotRepository = timeSlotRepository;
+    }
 
     @Override
     public AvailabilityDTO[] getDailyAvailability(long activityId, LocalDate date) {
