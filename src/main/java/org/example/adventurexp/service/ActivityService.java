a75d640e962ae2331514649bd3a093e5a617dea6
Joakim Rosdahl Severinsen <jose0001@stud.ek.dk>
2025-10-02 15:46:13 +0200

    Har lavet lidt, men skal lige tilbage på development for at hjælpe Meller.

---
+++ src/main/java/org/example/adventurexp/service/ActivityService.java
@@ -0,0 +1,20 @@
+package org.example.adventurexp.service;
+
+import org.example.adventurexp.model.Activity;
+import org.example.adventurexp.repository.IActivityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityService implements IActivityService {
+
+    org.example.adventurexp.repository.IActivityRepository activityRepository;
+
+    public ActivityService(IActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    @Override
+    public Activity createActivity(Activity activity) {
+        return null;
+    }
+}
---
+++ src/main/java/org/example/adventurexp/service/IActivityService.java
@@ -0,0 +1,7 @@
+package org.example.adventurexp.service;
+
+import org.example.adventurexp.model.Activity;
+
+public interface IActivityService {
+    Activity createActivity(Activity activity);
+}
