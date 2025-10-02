7e92fe7d551ef75518f66e6a432e26a0d39e6105
Joakim Rosdahl Severinsen <jose0001@stud.ek.dk>
2025-10-02 15:25:37 +0200

    Oprettet ActivityService kodet op mod interfacet IActivityService og tilføjet metoden findAll(), hvilket teknisk set er tasken SCRUM-195.

---
+++ src/main/java/org/example/adventurexp/service/ActivityService.java
@@ -0,0 +1,23 @@
+package org.example.adventurexp.service;
+
+import org.example.adventurexp.model.Activity;
+import org.example.adventurexp.repository.IActivityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ActivityService implements IActivityService {
+
+    IActivityRepository activityRepository;
+
+    public ActivityService(IActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+
+    @Override
+    public List<Activity> findAll() {
+        return activityRepository.findAll();
+    }
+}
