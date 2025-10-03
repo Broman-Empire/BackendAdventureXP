2631c020b158a7e4e3ec361a3201d0bc57fa05c1
Sofie Broman <sofierytter@hotmail.com>
2025-10-03 11:23:54 +0200

    rettede metode i repo til optional. JPA bruger optionals

--- src/main/java/org/example/adventurexp/Repository/ITimeSlotRepository.java
+++ src/main/java/org/example/adventurexp/Repository/ITimeSlotRepository.java
@@ -3,10 +3,15 @@ package org.example.adventurexp.repository;
 import org.example.adventurexp.model.Activity;
 import org.example.adventurexp.model.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ITimeSlotRepository extends JpaRepository<TimeSlot, Long> {
 
-    List<TimeSlot> findByActivity(Activity activity);
+    Optional<List<TimeSlot>> findByActivity(Activity activity);
+
+    Optional<TimeSlot> findByActivityAndStartsAt(Activity activity, LocalDateTime startsAt);
 
 }
