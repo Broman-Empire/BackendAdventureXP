7f2b60f23a2801d794106976d57002b766f9ad54
Mellerkaer <rasmusmellerkaer@gmail.com>
2025-10-03 17:37:40 +0200

    tilføjet endpoint

--- src/main/java/org/example/adventurexp/controller/ActivityController.java
+++ src/main/java/org/example/adventurexp/controller/ActivityController.java
@@ -1,26 +1,35 @@
 package org.example.adventurexp.controller;
 
-import org.example.adventurexp.model.Activity;
+import org.example.adventurexp.dto.ActivityDTO;
+import org.example.adventurexp.dto.AvailabilityDTO;
 import org.example.adventurexp.service.IActivityService;
+import org.example.adventurexp.service.IAvailabilityService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
-
 @RestController
-
-
-@RequestMapping("/api/activities")
+@CrossOrigin("*")
+@RequestMapping("/actrivities")
 public class ActivityController {
 
     private final IActivityService activityService;
+    private final IAvailabilityService availabilityService;
 
-    public ActivityController(IActivityService activityService) {
+    public ActivityController(IActivityService activityService, IAvailabilityService availabilityService) {
         this.activityService = activityService;
+        this.availabilityService = availabilityService;
     }
 
-    public List<Activity> getAllActivities() {
+    @GetMapping("/all")
+    public List<ActivityDTO> getAllActivities() {
         return activityService.findAll();
     }
 
+    @GetMapping("/availability")
+    public AvailabilityDTO[] getAvailability(@RequestParam("activityId") long activityId, @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        return availabilityService.getDailyAvailability(activityId, date);
+    }
 }
