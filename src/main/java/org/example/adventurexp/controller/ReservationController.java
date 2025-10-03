290fc1e00df5232be0a040463bdd6e9b276ecab7
bixson <alexanderornb@gmail.com>
2025-10-03 20:11:28 +0200

    Implemented createReservation().ReservationController.java

--- src/main/java/org/example/adventurexp/controller/ReservationController.java
+++ src/main/java/org/example/adventurexp/controller/ReservationController.java
@@ -1,10 +1,30 @@
 package org.example.adventurexp.controller;
 
+import org.example.adventurexp.dto.CreateReservationDTO;
+import org.example.adventurexp.dto.ReservationResponse;
+import org.example.adventurexp.service.IReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
+
+    private final IReservationService reservationService;
+
+    @Autowired
+    public ReservationController (IReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResponse> createReservation(@RequestBody CreateReservationDTO request) {
+        ReservationResponse response = reservationService.createReservation(request);
+        return ResponseEntity.ok(response);
+    }
 
 }
