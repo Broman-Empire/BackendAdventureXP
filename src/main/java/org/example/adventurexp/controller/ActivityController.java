357e39be55b0921682e19ad0c7520d4e16938e25
Joakim Rosdahl Severinsen <jose0001@stud.ek.dk>
2025-10-03 09:40:28 +0200

    Rettet til i ActivityController m+ pulled ActivityService fra development.

--- src/main/java/org/example/adventurexp/controller/ActivityController.java
+++ src/main/java/org/example/adventurexp/controller/ActivityController.java
@@ -1,14 +1,23 @@
 package org.example.adventurexp.controller;
 
+import org.example.adventurexp.model.Activity;
+import org.example.adventurexp.service.IActivityService;
 import org.springframework.web.bind.annotation.*;
 
-private final IActivityService activityService;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/activities")
 public class ActivityController {
 
-    public String getAllActivities() {
+    private final IActivityService activityService;
+
+    public ActivityController(IActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    public List<Activity> getAllActivities() {
         return activityService.findAll();
     }
 
