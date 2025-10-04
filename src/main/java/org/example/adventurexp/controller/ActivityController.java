2c1fd29bf6f271d783ada457b8d8fdff34a8e512
Sofie Broman <sofierytter@hotmail.com>
2025-10-04 20:19:15 +0200

    tilføjet gammelt endpoint til ActivityController

--- src/main/java/org/example/adventurexp/controller/ActivityController.java
+++ src/main/java/org/example/adventurexp/controller/ActivityController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 @RestController
-
+@RequestMapping("/api/activities")
 public class ActivityController {
 
     private final IActivityService activityService;
@@ -20,7 +20,7 @@ public class ActivityController {
     }
 
 
-    @GetMapping("/activities")
+    @GetMapping("")
     public List<ActivityDTO> getAllActivities() {
         return activityService.findAll();
     }
