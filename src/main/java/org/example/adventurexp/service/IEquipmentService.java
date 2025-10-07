ce3aea75339962e58507e489ac8f57f9774ab15f
bixson <alexanderornb@gmail.com>
2025-10-07 08:49:28 +0200

    Changed addEquipment() from 'void' to 'Equipment' in EquipmentService interface.

--- src/main/java/org/example/adventurexp/service/IEquipmentService.java
+++ src/main/java/org/example/adventurexp/service/IEquipmentService.java
@@ -16,7 +16,7 @@ public interface IEquipmentService {
 
     List<Equipment> listByActivity(long activityId);
 
-    void addEquipment(long activityId, Equipment equipment);
+    Equipment addEquipment(long activityId, Equipment equipment);
 
     Equipment updateEquipment(long equipmentId, Equipment patch);
 
