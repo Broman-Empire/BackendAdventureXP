33937e4704ee89de1396f0c7304d11d62415a9fc
Nima <nimaginobilisalami@gmail.com>
2025-10-08 13:59:42 +0200

    tilføjet en @Query til findByBookingDate() i ReservationRepo

--- src/main/java/org/example/adventurexp/repository/IReservationRepository.java
+++ src/main/java/org/example/adventurexp/repository/IReservationRepository.java
@@ -2,6 +2,7 @@ package org.example.adventurexp.repository;
 
 import org.example.adventurexp.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,4 +14,12 @@ public interface IReservationRepository extends JpaRepository<Reservation, Long>
 
 
     List<Reservation> findByPhone(String phone);
+
+    @Query("""
+        SELECT DISTINCT r FROM Reservation r
+        JOIN r.bookings b
+        JOIN b.timeSlot t
+        WHERE DATE(t.startsAt) = :date
+    """)
+    List<Reservation> findByBookingDate(LocalDate date);
 }
