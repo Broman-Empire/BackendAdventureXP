0377cb6ee9a04c285d47505f90f997b51a7f63ab
Sofie Broman <sofierytter@hotmail.com>
2025-10-09 10:41:54 +0200

    Ãtslettede deleteReservation() fra reservation controller, demn låå i adminController

--- src/main/java/org/example/adventurexp/controller/ReservationController.java
+++ src/main/java/org/example/adventurexp/controller/ReservationController.java
@@ -35,10 +35,5 @@ public class ReservationController {
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping("/admin/reservations/{id}")
-    public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
-        reservationService.deleteReservation(id);
-        return ResponseEntity.noContent().build(); // 204 No Content
-    }
 
 }
