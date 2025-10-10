37958db0ff9bf3cf6cde04ddfc9d3d53a6b3be40
Nima <nimaginobilisalami@gmail.com>
2025-10-10 12:22:39 +0200

    tilføjet @JsonIgnore til Activity relationen i TimeSlot

--- src/main/java/org/example/adventurexp/model/TimeSlot.java
+++ src/main/java/org/example/adventurexp/model/TimeSlot.java
@@ -1,5 +1,6 @@
 package org.example.adventurexp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -25,6 +26,7 @@ public class TimeSlot {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "activity_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
     private Activity activity;
 
 
